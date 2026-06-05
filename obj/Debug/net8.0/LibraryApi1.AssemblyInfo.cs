039b228558e7427e488cfbac629a57487f143dc1
somaya mousa <shaymaadarmousa@gmail.com>
2026-06-05 17:11:25 +0300

    Initial commit

--- obj/Debug/net8.0/LibraryApi1.AssemblyInfo.cs
+++ obj/Debug/net8.0/LibraryApi1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryApi1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63495ecab6abbb0bfb58fe56b2bdc04fc7bab1cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryApi1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryApi1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
